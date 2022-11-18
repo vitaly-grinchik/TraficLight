e15f6e993d378682a9f766faf1e1c57598eec4e6
Vitaly Grinchik <vitaly.grinchik@gmail.com>
2022-11-18 22:03:07 +0300

    Initial Commit

---
+++ TraficLight/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TraficLight
+//
+//  Created by Виталий Гринчик on 18.11.22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
