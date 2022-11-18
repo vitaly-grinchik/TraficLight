a156f3a0f07f6fd820573c37b311cf7e4bfce97c
Vitaly Grinchik <vitaly.grinchik@gmail.com>
2022-11-19 00:19:29 +0300

    - Add view as body of a traffic light containing stack of color lights. - Reassigning of all constraints

--- TraficLight/ViewController.swift
+++ TraficLight/ViewController.swift
@@ -9,11 +9,27 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    // MARK: - IB Outlets
+    @IBOutlet weak var redView: UIView!
+    @IBOutlet weak var yellowView: UIView!
+    @IBOutlet weak var greenView: UIView!
+    @IBOutlet weak var startButton: UIButton!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        redView.layer.cornerRadius = 80
+        yellowView.layer.cornerRadius = 80
+        greenView.layer.cornerRadius = 80
+        
+        redView.alpha = 0.4
+        yellowView.alpha = 0.4
+        greenView.alpha = 0.4
     }
 
-
+    // MARK: - IB Actions
+    @IBAction func startButtonTaped() {
+    }
+    
 }
 
