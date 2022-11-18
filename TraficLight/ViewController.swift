49080f06d683dfb0b0b2bc3767be9b542deb6b6d
Vitaly Grinchik <vitaly.grinchik@gmail.com>
2022-11-19 01:07:10 +0300

    Button configuration

--- TraficLight/ViewController.swift
+++ TraficLight/ViewController.swift
@@ -10,21 +10,33 @@ import UIKit
 class ViewController: UIViewController {
 
     // MARK: - IB Outlets
+    @IBOutlet weak var trafficLightBodyView: UIView!
     @IBOutlet weak var redView: UIView!
     @IBOutlet weak var yellowView: UIView!
     @IBOutlet weak var greenView: UIView!
     @IBOutlet weak var startButton: UIButton!
     
-    
     override func viewDidLoad() {
         super.viewDidLoad()
+        // Initial configuration of trafic light body and colors
         redView.layer.cornerRadius = 80
         yellowView.layer.cornerRadius = 80
         greenView.layer.cornerRadius = 80
         
-        redView.alpha = 0.4
+        redView.alpha = 1
         yellowView.alpha = 0.4
         greenView.alpha = 0.4
+        
+        trafficLightBodyView.layer.cornerRadius = 20
+        trafficLightBodyView.layer.borderWidth = 4
+        trafficLightBodyView.layer.borderColor = UIColor.black.cgColor
+        
+        // Button configuration
+        startButton.backgroundColor = .systemBlue
+        startButton.setTitleColor(.white, for: .normal)
+        startButton.layer.cornerRadius = 10
+        startButton.contentEdgeInsets.left = 15
+        startButton.contentEdgeInsets.right = 15
     }
 
     // MARK: - IB Actions
