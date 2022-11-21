080b6465f7bd0d45a75a1952d2f772232d031366
Vitaly Grinchik <vitaly.grinchik@gmail.com>
2022-11-21 17:02:58 +0300

    Delete "private" modifiers

--- TraficLight/ViewController.swift
+++ TraficLight/ViewController.swift
@@ -16,13 +16,13 @@ class ViewController: UIViewController {
     @IBOutlet weak var greenView: UIView!
     @IBOutlet weak var startButton: UIButton!
     
-    var activeLightIndex = 2                            // To start with Red light
+    var activeLightIndex = 2                    // To start with Red light
     
-    private var lightsStatus = [false, false, false]    // Init status of all lights - OFF
+    var lightsStatus = [false, false, false]    // Init status of all lights - OFF
     
-    private let lightOnAlpha: CGFloat = 1.0             // Brightness of light if ON
+    let lightOnAlpha: CGFloat = 1.0             // Brightness of light if ON
     
-    private let lightOffAlpha: CGFloat = 0.2            // Brightness of light if OFF
+    let lightOffAlpha: CGFloat = 0.2            // Brightness of light if OFF
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -59,7 +59,7 @@ class ViewController: UIViewController {
         updateLights()
     }
     
-    private func updateLights() {
+    func updateLights() {
         redView.alpha = lightsStatus[0] ? lightOnAlpha : lightOffAlpha
         yellowView.alpha = lightsStatus[1] ? lightOnAlpha : lightOffAlpha
         greenView.alpha = lightsStatus[2] ? lightOnAlpha : lightOffAlpha
